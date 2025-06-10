7b35ba8ee7b88e18fcf62f0783f18bdbe77f0bb5
KartikVermaIIT <kartikverma2005@.com>
2025-06-11 03:26:05 +0530

    Fix Vite config for correct dist build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss';
-// https://vitejs.dev/config/
+import tailwindcss from 'tailwindcss'
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  // base: '/AlgoViz/'
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist'
+  }
 })
