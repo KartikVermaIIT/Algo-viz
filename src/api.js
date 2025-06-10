f41655410f5e15f55c993a0d60d4781d7db22b0d
KartikVermaIIT <kartikverma2005@.com>
2025-06-11 02:58:28 +0530

    Add .gitattributes to normalize line endings

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'http://127.0.0.1:8000/',
-    baseURL: 'https://algoviz-api-3.onrender.com'
+    baseURL: 'https://algo-visualiser-7uku.onrender.com'
 });
 
 export default api;
